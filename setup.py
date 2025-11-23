da379a0440fb8b5c72f6f6dd78b3e622e53287e6
Bùi Minh Đức <duc@ducmbui.com>
2025-11-23 20:47:22 +0700

    Add setup.py to install from pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TwoFish',
+    version='1.0.0',
+    description='Pure Python TwoFish implementation',
+    author='K-Czaplicki',
+    packages=find_packages(),
+    py_modules=['TwoFish'], 
+)
